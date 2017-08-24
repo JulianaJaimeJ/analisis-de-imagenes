c7c8083bd78cacc3351bea421a05d0668bb3042f
MARIA JULIANA JAIME JAIMES <mj.jaime10@sis.virtual.uniandes.edu.co>
2017-08-24 10:05:11 -0500

    avance de la semana pasada

--- laboratorio1.m
+++ laboratorio1.m
@@ -1,15 +1,59 @@
-%%5.1
-x = imread('cat.jpg');
-imshow(x);
+%% primera parte
+x=imread('images.jpeg');
+figure(1)
+imshow(x)
 pause
+%plot(x)
+%obtencion del histograma
+I=rgb2gray(x);
 
-I = rgb2gray(x);
-imshow(I);
+figure(2)
+imshow(I)
 pause
-
-imhist(x);
+%imhist(x)
 pause
+%histogram(hist,255)
 imhist(I);
 pause
 
-%%5.2
+r=x(:,:,1);
+g=x(:,:,2);
+blue=x(:,:,3);
+
+figure(3)
+
+subplot(2,2,1)
+title('original')
+imshow(x)
+subplot(2,2,2)
+title('rojos')
+imshow(r)
+subplot(2,2,3)
+title('verdes')
+imshow(g)
+subplot(2,2,4)
+title('Azules')
+imshow(blue)
+
+%% parte 2
+clc
+z=imread('tumor.png');
+z1=rgb2gray(z);
+[b,n]=imhist(z,100);
+%recreacion imagen original
+figure(4)
+bar(b)
+figure(5)
+imshow(z1)
+%descompocicion de la imagen
+
+%recreacion por medio de toolbox de matlab
+zn=im2bw(z1,0.6);
+figure(6)
+imshow(zn)
+%codigo de recreacion
+pos=find(z1>=250);
+
+znew=z1(pos);
+figure(7)
+imshow(znew)
