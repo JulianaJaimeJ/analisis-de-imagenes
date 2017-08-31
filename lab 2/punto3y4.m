c9044a90d6df5342e6114cb0dc47b0d8c8f259dc
EduardoFonseca10 <ea.fonseca10@uniandes.edu.co>
2017-08-30 21:32:58 -0500

    lab modify
    punto 5.4

--- lab 2/punto3y4.m
+++ lab 2/punto3y4.m
@@ -1,5 +1,7 @@
 %% 5.3 Visualizar los canales de un MRI
-
+clc
+close all
+clear all
 %Se leen los metadatos de la modalidad T1 para hallar el valor de las
 %filas, columnas y cortes
 metadata = nii_read_header('Brats17_2013_10_1_t1\Brats17_2013_10_1_t1.nii');
@@ -28,7 +30,6 @@ end
 
 %Se llena el campo de T1ce en vol
 %Se lee el archivo de la modalidad T1ce
-
 
 %Para cada corte en la matriz de T1 se transfieren los datos a vol
 for k=1:cortes%Recorre todos los cortes
@@ -66,14 +67,18 @@ end
 %Se muestran las 4 modalidades al tiempo
 for i =1:cortes %Se recorren todos los cortes
     %Se divide el plano en 4 espacios para cada modalidad
-    subplot(2,2,1, 'T1'); 
+    subplot(2,2,1); 
     imshow(vol(:,:,i,1), [200 500]); %Se muestra la modalidad T1
-    subplot(2,2,2,'T1ce');
+    title('T1')
+    subplot(2,2,2);
     imshow(vol(:,:,i,2), [200 600]);%Se muestra la modalidad T1ce
-    subplot(2,2,3,'T2');
+    title('T1ce')
+    subplot(2,2,3);
     imshow(vol(:,:,i,3), [100 800]);%Se muestra la modalidad T2
-    subplot(2,2,4,'Flair');
+    title('T2')
+    subplot(2,2,4);
     imshow(vol(:,:,i,4), [0 600]);%Se muestra la modalidad Flair
+    title('Flair')
     drawnow
 end
 
@@ -120,5 +125,49 @@ rutaFlair = strcat(carpetaFlair, '\', carpetaFlair, extension) %Se costruye la r
 % mriFlair= nii_read_volume(rutaFlair);
 
 %% 5.4 Visualizar los canales de un MRI
+clc
+clear all
+%Se leen los metadatos de la modalidad T1 para hallar el valor de las
+%filas, columnas y cortes
+metadata = nii_read_header('Brats17_2013_10_1_seg\Brats17_2013_10_1_seg.nii');
+filas= metadata.Dimensions(1);
+columnas= metadata.Dimensions(2);
+cortes= metadata.Dimensions(3);
+modalidades= metadata.Dimensions(4)+metadata.Dimensions(5)+metadata.Dimensions(6)+metadata.Dimensions(7);
+vol = single(zeros(filas, columnas,cortes, modalidades));
 
-mriFlair= nii_read_volume('Brats17_2013_10_1_flair\Brats17_2013_10_1_flair.nii');
+mriSeg= nii_read_volume('Brats17_2013_10_1_seg\Brats17_2013_10_1_seg.nii');
+
+%Se llena el campo de t1 en vol
+for k=1:cortes %Recorre todos los cortes
+    for i=1:filas %Recorre todas las filas
+        for j=1:columnas %Recorre todas las columnas
+            vol(i,j,k,1) = mriSeg(i,j,k); %Se trasfiere la información de seg a vol
+        end
+        end
+end
+
+[an,al,pr]=size(vol);
+for i =1:pr %Se recorren todos los cortes
+    s=vol(:,:,i);
+    %Se divide el plano en 4 espacios para cada modalidad
+    a=subplot(2,2,1); 
+    colormap(a,autumn);
+    imshow(s,[]); %Se muestra la modalidad autumn
+    drawnow
+    
+    b=subplot(2,2,2);
+    colormap(b,winter);
+    imshow(s,[]);%Se muestra la modalidad winter 
+    drawnow
+    
+    c=subplot(2,2,3);
+    colormap(c,spring);
+    imshow(s,[]);%Se muestra la modalidad spring
+    drawnow
+    
+    d=subplot(2,2,4);
+    colormap(d,summer);
+    imshow(s,[]);%Se muestra la modalidad summer
+    drawnow
+end
