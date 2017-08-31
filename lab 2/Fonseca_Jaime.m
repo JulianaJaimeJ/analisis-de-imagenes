1bb3ff8260713ab82413eb5e88064958065d2f6f
JulianaJaimeJ <mj.jaime10@uniandes.edu.co>
2017-08-30 23:06:41 -0500

    Final
    Junté todos los puntos en un solo archivo con el nombre Fonseca_Jaime para entregar. Corrí el 5.4 y sirve muy bien

--- lab 2/Fonseca_Jaime.m
+++ lab 2/Fonseca_Jaime.m
@@ -1,3 +1,34 @@
+%% 5.2 Tipos de Imágenes
+clc;clear all;close all
+archivo=dir(fullfile(toolboxdir('images'),'imdata'));
+
+archivo(1)=[];
+archivo(1)=[];
+% parte 1
+img=imread(archivo(14).name);imshow(img);pause
+imgn=im2bw(img,0.5);imshow(imgn);pause
+% parte 2
+lua=imread(archivo(53).name);imshow(lua);pause
+luna=im2bw(lua,0.09);imshow(luna);pause
+
+% parte 3
+load trees
+figure(2)
+subplot(1,2,1)
+imagesc(X)
+subplot(1,2,2)
+image(X)
+pause
+
+figure(3)
+subplot(1,2,1)
+imshow(X,map)
+subplot(1,2,2)
+image(X);colormap(map);colorbar;
+%parte 4
+rgb=ind2rgb(X,map);
+imwrite(rgb,'Punto4.png');
+
 %% 5.3 Visualizar los canales de un MRI
 clc
 close all
--- lab 2/VizualizarAnotaciones.m
+++
@@ -1,3 +0,0 @@
-clc;close all;clear all
-metadata=nii_read_header('Brats17_2013_10_1_seg\Brats17_2013_10_1_seg.nii');
-mril=nii_read_volume('Brats17_2013_10_1_seg\Brats17_2013_10_1_seg.nii');
